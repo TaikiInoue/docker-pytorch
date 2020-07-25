e307d82056a5b74afad4241eb0e2d5d7e44875b1
TaikiInoue <taikiinoue45@gmail.com>
2020-07-26 01:26:07 +0900

    Made the default shell zsh

--- 10.2-cudnn7-devel-ubuntu18.04/docker_run.sh
+++ 10.2-cudnn7-devel-ubuntu18.04/docker_run.sh
@@ -1,10 +1,10 @@
 docker run --runtime nvidia -it --rm \
     --network host \
-    --volume ~/github:/app/github \
-    --volume /home/inoue/.ssh:/root/.ssh \
-    --volume /home/inoue/.zshrc:/root/.zshrc \
-    --workdir /app \
-    --name torch \
-    --hostname torch \
+    --volume /home/$USER/github:/root/github \
+    --volume /home/$USER/.ssh:/root/.ssh \
+    --volume /home/$USER/.zshrc:/root/.zshrc \
+    --workdir /root \
+    --name metis-docker \
+    --hostname metis-docker \
     taikiinoue45/pytorch:10.2-cudnn7-devel-ubuntu18.04 \
-    /bin/bash
+    /usr/bin/zsh
