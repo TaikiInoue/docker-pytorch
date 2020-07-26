76f63744ecad359c1ef0d7427bb573d96b1ddda0
TaikiInoue <taikiinoue45@gmail.com>
2020-07-26 23:51:25 +0900

    Complete tmux setting

--- 10.2-cudnn7-devel-ubuntu18.04/run.sh
+++ 10.2-cudnn7-devel-ubuntu18.04/run.sh
@@ -2,8 +2,6 @@ docker run --runtime nvidia -it --rm \
     --network host \
     --volume /home/$USER/github:/root/github \
     --volume /home/$USER/.ssh:/root/.ssh \
-    --volume /home/$USER/.zshrc:/root/.zshrc \
-    --workdir /root \
     --name metis-docker \
     --hostname metis-docker \
     taikiinoue45/pytorch:10.2-cudnn7-devel-ubuntu18.04 \
