0fa9e58da88aa152af9394c6fc9013e0bc06573e
TaikiInoue <taikiinoue45@gmail.com>
2020-07-27 18:33:48 +0900

    Improve how to share .ssh folder

--- 10.2-cudnn7-devel-ubuntu18.04/entrypoint.sh
+++ 10.2-cudnn7-devel-ubuntu18.04/entrypoint.sh
@@ -1,4 +1,8 @@
 cp -r /tmp/.ssh /root/.ssh
-mkdir -p /dgx
-sshfs inoue.momo:/dgx/ /dgx/
+mkdir -p /dgx/github
+mkdir -p /dgx/data
+mkdir -p /dgx/shared
+sshfs inoue.momo:/dgx/inoue/github/ /dgx/github
+sshfs inoue.momo:/dgx/inoue/data/ /dgx/data
+sshfs inoue.momo:/dgx/shared/ /dgx/shared/
 /usr/bin/zsh
