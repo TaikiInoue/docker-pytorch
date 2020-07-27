5e8dbf1823f7a0ce96f4b38a9e391c18e6579ba9
TaikiInoue <taikiinoue45@gmail.com>
2020-07-27 17:54:28 +0900

    Improve how to share .ssh folder

---
+++ 10.2-cudnn7-devel-ubuntu18.04/entrypoint.sh
@@ -0,0 +1,4 @@
+cp -r /tmp/.ssh /root/.ssh
+mkdir -p /dgx
+sshfs inoue.momo:/dgx/ /dgx/
+/usr/bin/zsh
--- 10.2-cudnn7-devel-ubuntu18.04/run.sh
+++ 10.2-cudnn7-devel-ubuntu18.04/run.sh
@@ -1,8 +1,7 @@
-docker run --runtime nvidia -it --rm \
+docker run --runtime nvidia -it --rm --privileged \
     --network host \
-    --volume /home/$USER/github:/root/github \
-    --volume /home/$USER/.ssh:/root/.ssh \
+    --volume /home/$USER/.ssh:/tmp/.ssh \
+    --workdir /dgx \
     --name metis-docker \
     --hostname metis-docker \
-    taikiinoue45/pytorch:10.2-cudnn7-devel-ubuntu18.04 \
-    /usr/bin/zsh
+    taikiinoue45/pytorch:10.2-cudnn7-devel-ubuntu18.04
