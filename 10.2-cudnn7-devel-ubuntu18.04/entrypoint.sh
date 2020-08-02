9fa70b9e99d6e46b8d81c1b49181f431596b731d
TaikiInoue <taikiinoue45@gmail.com>
2020-08-02 10:55:54 +0900

    Modified hydra and streamlit settings

--- 10.2-cudnn7-devel-ubuntu18.04/entrypoint.sh
+++ 10.2-cudnn7-devel-ubuntu18.04/entrypoint.sh
@@ -1,8 +1,9 @@
+sed -i "s/console, file/file/" /opt/conda/lib/python3.6/site-packages/hydra/conf/hydra/job_logging/default.yaml
 cp -r /tmp/.ssh /root/.ssh
 mkdir -p /dgx/github
 mkdir -p /dgx/data
-mkdir -p /dgx/shared
+mkdir -p /dgx/shared/momo
 sshfs inoue.momo:/dgx/inoue/github/ /dgx/github
 sshfs inoue.momo:/dgx/inoue/data/ /dgx/data
-sshfs inoue.momo:/dgx/shared/ /dgx/shared/
+sshfs inoue.momo:/dgx/shared/ /dgx/shared/momo
 /usr/bin/zsh
