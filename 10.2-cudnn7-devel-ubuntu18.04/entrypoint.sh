37a44faf6ab4ad0e7954b185b8c34372fcfbf28f
TaikiInoue <taikiinoue45@gmail.com>
2020-08-03 17:25:00 +0900

    Sync with conf files in host

--- 10.2-cudnn7-devel-ubuntu18.04/entrypoint.sh
+++ 10.2-cudnn7-devel-ubuntu18.04/entrypoint.sh
@@ -1,5 +1,9 @@
+cp /tmp/.ssh /root/.ssh -r
+cp /tmp/.zshrc /root/.zshrc
+cp /tmp/.tmux.conf /root/.tmux.conf
 sed -i "s/console, file/file/" /opt/conda/lib/python3.6/site-packages/hydra/conf/hydra/job_logging/default.yaml
-cp -r /tmp/.ssh /root/.ssh
+sed -i "s/.hydra/hydra/" /opt/conda/lib/python3.6/site-packages/hydra/conf/hydra.yaml
+sed -i "s/%F{red}%M@%n%f/%F{blue}%M@%n%f/" /root/.zshrc
 mkdir -p /dgx/github
 mkdir -p /dgx/data
 mkdir -p /dgx/shared/momo
