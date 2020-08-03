b15915940afc206284521fa1de06151d2d10522b
TaikiInoue <taikiinoue45@gmail.com>
2020-08-03 17:23:05 +0900

    Combine build.sh and run.sh into Makefile

--- 10.2-cudnn7-devel-ubuntu18.04/build.sh
+++
@@ -1,1 +0,0 @@
-docker build -t taikiinoue45/pytorch:10.2-cudnn7-devel-ubuntu18.04 .
--- 10.2-cudnn7-devel-ubuntu18.04/run.sh
+++
@@ -1,7 +0,0 @@
-docker run --runtime nvidia -it --rm --privileged \
-    --network host \
-    --volume /home/$USER/.ssh:/tmp/.ssh \
-    --workdir /dgx \
-    --name metis-docker \
-    --hostname metis-docker \
-    taikiinoue45/pytorch:10.2-cudnn7-devel-ubuntu18.04
