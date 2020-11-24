fcad2e7fd96d5e498b2aee864708cf4fa86014f3
TaikiInoue <taikiinoue45@gmail.com>
2020-11-24 09:53:37 +0900

    Some modification

--- 10.2-cudnn7-devel-ubuntu18.04/entrypoint.sh
+++ 10.2-cudnn7-devel-ubuntu18.04/entrypoint.sh
@@ -2,7 +2,6 @@ cp /tmp/.ssh /root/.ssh -r
 cp /tmp/.zshrc /root/.zshrc
 cp /tmp/.tmux.conf /root/.tmux.conf
 sed -i "s/console, file/file/" /opt/conda/lib/python3.6/site-packages/hydra/conf/hydra/job_logging/default.yaml
-sed -i "s/.hydra/hydra/" /opt/conda/lib/python3.6/site-packages/hydra/conf/hydra.yaml
 sed -i "s/%F{red}%M@%n%f/%F{blue}%M@%n%f/" /root/.zshrc
 mkdir -p /dgx/github
 mkdir -p /dgx/data
