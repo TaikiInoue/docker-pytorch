936d77b331a355729638ef249426f4c2f2d24c44
TaikiInoue <taikiinoue45@gmail.com>
2020-11-24 10:41:51 +0900

    Fix bug in sshfs

--- 10.2-cudnn7-devel-ubuntu18.04/entrypoint.sh
+++ 10.2-cudnn7-devel-ubuntu18.04/entrypoint.sh
@@ -6,7 +6,6 @@ sed -i "s/%F{red}%M@%n%f/%F{blue}%M@%n%f/" /root/.zshrc
 mkdir -p /dgx/github
 mkdir -p /dgx/data
 mkdir -p /dgx/shared/momo
-sshfs inoue.momo:/dgx/inoue/github/ /dgx/github
-sshfs inoue.momo:/dgx/inoue/data/ /dgx/data
-sshfs inoue.momo:/dgx/shared/ /dgx/shared/momo
+sshfs inoue@dgx-momo.office.nablas.com:/dgx/inoue/github/ /dgx/github
+sshfs inoue@dgx-momo.office.nablas.com:/dgx/shared/ /dgx/shared/momo
 /usr/bin/zsh
